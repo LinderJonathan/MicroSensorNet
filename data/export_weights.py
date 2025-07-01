b59f93c3037a62150c25dfab582329cb3663839b
LinderJonathan <linderjonathan3@gmail.com>
2025-07-01 23:43:29 +0200

    Finnished script to export weights to JSON. TODO: cleanup, c++ inference

--- data/export_weights.py
+++ data/export_weights.py
@@ -1,23 +1,41 @@
 import argparse
 import torch
 import os
+import numpy
 from classifier import NN_MSN
-def get_weights(model_path):
+import json
+
+FRAC_BITS = 8
+
+def quantize_weights(weights):
+    scale = 1 << FRAC_BITS
+    return (scale * weights).round().astype(int).tolist()
+
+def export_weights(model_path, weight_path):
+
     state_dict = torch.load(model_path)
 
     model = NN_MSN()
     model.load_state_dict(state_dict)
 
-    return model
-def main(model_path):
-    model = get_weights(model_path)
+    all_weights = {}
     for name, param in model.named_parameters():
-        print(f"name: {name}")
-        print(f"param: {param}")
+
+        params = quantize_weights(state_dict[name].numpy())
+        all_weights[name] = params
+
+    with open(weight_path, "w") as f:
+            json.dump(all_weights, f, indent=4)
+
+    return state_dict
+def main(model_path, weight_path):
+    export_weights(model_path=model_path, weight_path=weight_path)
+
     return 0
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('model_path', type=str, help='Path to saved model')
+    parser.add_argument('weight_path', type=str, help='Path to JSON file for weights')
     args = parser.parse_args()
-    main(args.model_path)
+    main(args.model_path, args.weight_path)
