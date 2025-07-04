95cde162e4e4ec2cb2f6a9330b96d195fe336ad3
LinderJonathan <linderjonathan3@gmail.com>
2025-07-04 23:16:18 +0200

    oops

--- src/neural_network.cpp
+++ src/neural_network.cpp
@@ -6,15 +6,17 @@
 // TODO: instantiate class
 
 std::vector<fixed_point_t> NeuralNetwork::forward(const std::vector<fixed_point_t>& input){
-    
-    // TODO: iterate over the layers in the network,
-    for (auto& l : layers) {
-        std::vector<fixed_point_t> input = l.layer_forward(input);
-        relu(input);
-    }
 
-    return {0};
+    std::vector<fixed_point_t> x = layers[0].layer_forward(input);
+    relu(x);
 
+    x = layers[1].layer_forward(x);
+    relu(x);
+
+    x = layers[2].layer_forward(x);
+    relu(x);
+
+    return x;
 }
 
 std::vector<fixed_point_t> NeuralNetwork::relu(std::vector<fixed_point_t>& input){
