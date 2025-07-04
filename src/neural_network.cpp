cdb35e281016d0c8d53473bbbfbd4caedb9f1bd4
LinderJonathan <linderjonathan3@gmail.com>
2025-07-04 23:14:45 +0200

    c++ forwarding completed

--- src/neural_network.cpp
+++ src/neural_network.cpp
@@ -1,4 +1,24 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 #include "neural_network.hpp"
 
+// TODO: instantiate class
+
+std::vector<fixed_point_t> NeuralNetwork::forward(const std::vector<fixed_point_t>& input){
+    
+    // TODO: iterate over the layers in the network,
+    for (auto& l : layers) {
+        std::vector<fixed_point_t> input = l.layer_forward(input);
+        relu(input);
+    }
+
+    return {0};
+
+}
+
+std::vector<fixed_point_t> NeuralNetwork::relu(std::vector<fixed_point_t>& input){
+    for (auto& neuron : input) {
+        neuron = std::max(neuron, static_cast<fixed_point_t>(0));
+    }
+}
--- src/neural_network.hpp
+++ src/neural_network.hpp
@@ -2,20 +2,34 @@
 
 #include <iostream>
 #include <vector>
+#include <numeric>
 #include "fixed_point.hpp"
 
 struct DenseLayer {
 
-    std::vector<float> input;
+    std::vector<fixed_point_t>input;
     std::vector<std::vector<fixed_point_t>> weights;
     std::vector<fixed_point_t> bias;
+
+    std::vector<fixed_point_t> layer_forward(const std::vector<fixed_point_t>& input) {
+
+        std::vector<fixed_point_t> output(weights.size(), 0);
+        for (unsigned int i = 0; i < weights.size(); i++) {
+
+            fixed_point_t xw = std::inner_product(weights[i].begin(), weights[i].end(), input.begin(), 0);
+            output[i] = xw + bias[i];
+        }
+
+    }
+
+
 };
 
 class NeuralNetwork {
     public:
-        std::vector<DenseLayer> layer;
-        
+        std::vector<DenseLayer> layers;
         // Forward expects a vector of (x,y,z) coordinates, and produces 
         // a distribution over three classes (standing, walking, jogging)
-        std::vector<fixed_point_t> forward(std::vector<fixed_point_t> input);
+        std::vector<fixed_point_t> forward(const std::vector<fixed_point_t>& input);
+        std::vector<fixed_point_t> relu(std::vector<fixed_point_t>& input);
 };
