8341e98a8548b13021dc61a955528841639c49c6
LinderJonathan <linderjonathan3@gmail.com>
2025-07-06 19:25:04 +0200

    ignore this commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,13 @@
 #include "data.hpp"
+#include "neural_network.hpp"
 #include <iostream>
 
+NeuralNetwork NN;
+
 int main() {
+    // TODO: read in weigts and biases (possible to do at compile time and not runtime?
+    )
+    NN.layers = 
     std::cout << "test" << std::endl;    
     return 0;
 }
