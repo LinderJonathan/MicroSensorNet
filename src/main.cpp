567ef377b5f81a87f20298f769c7cda61ec54da4
Jonathan Linder <linderjonathan3@gmail.com>
2025-07-10 17:32:32 +0200

    huh

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include "neural_network.hpp"
+#include "inference.hpp"
 #include "nlohmann/json.hpp"
 #include <iostream>
 #include <fstream>
@@ -17,7 +18,14 @@ int main() {
     NN.load_weights(data);
 
     // Load test data and perform inference
-    
+    std::tuple<std::vector<std::string>, std::vector<std::vector<float>>> dataCSV = readCSV(INFERENCE_DATA_PATH);
+
+    std::vector<std::string> labels = std::get<0>(dataCSV); 
+    std::vector<std::vector<float>> data = std::get<1>(dataCSV);
+
+    for (auto& label : labels) {
+        std::cout << label << std::endl;
+    }
 
     // TODO: log neural network performance (MCU constraints, inference time, RAM usage, latency)
     return 0;
